a2dc85250eca26976e31dff8647a26ecfb064cb5
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-01-25 13:25:53 +0100

    Draw inner border and squares.

--- src/ChessboardWidget.cc
+++ src/ChessboardWidget.cc
@@ -22,7 +22,7 @@
 
 ChessboardWidget::ChessboardWidget()
 {
-  override_background_color(Gdk::RGBA("blue"));
+  // override_background_color(Gdk::RGBA("blue"));
 }
 
 ChessboardWidget::~ChessboardWidget()
@@ -34,16 +34,36 @@ bool ChessboardWidget::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
   const int width = get_allocation().get_width();
   const int height = get_allocation().get_height();
 
-  const int padding = 10;
-  const int border_size = 10;
+  const int padding = 15;
+  const int border_size = 30;
 
   const int board_size = std::min(width, height) - 2 * (padding + border_size);
   const int board_offset_x = (width - board_size) / 2;
   const int board_offset_y = (height - board_size) / 2;
 
+  const double square_size = board_size / 8.0;
+
   // Draw the background color.
   Gdk::Cairo::set_source_rgba(cr, get_style_context()->get_background_color());
   cr->paint();
+
+  // Draw the squares.
+  for (int x = 0; x < 8; x++)
+  {
+    for (int y = 0; y < 8; y++)
+    {
+      if ((x + y) % 2 == 0)
+      {
+        cr->set_source_rgba(0, 0, 1, 0.5);
+      }
+      else
+      {
+        cr->set_source_rgba(1, 1, 1, 0.5);
+      }
+      cr->rectangle(board_offset_x + x * square_size, board_offset_y + y * square_size, square_size, square_size);
+      cr->fill();
+    }
+  }
 
   // Draw the raised edge of the outer border.
   cr->set_source_rgba(1, 1, 1, 0.5);
@@ -57,6 +77,20 @@ bool ChessboardWidget::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
   cr->move_to(board_offset_x - border_size, board_offset_y + board_size + border_size);
   cr->line_to(board_offset_x + board_size + border_size, board_offset_y + board_size + border_size);
   cr->line_to(board_offset_x + board_size + border_size, board_offset_y - border_size);
+  cr->stroke();
+
+  // Draw the sunken edge of the inner border.
+  cr->set_source_rgba(0, 0, 0, 0.5);
+  cr->move_to(board_offset_x, board_offset_y + board_size);
+  cr->line_to(board_offset_x, board_offset_y);
+  cr->line_to(board_offset_x + board_size, board_offset_y);
+  cr->stroke();
+
+  // Draw the raised edge of the inner border.
+  cr->set_source_rgba(1, 1, 1, 0.5);
+  cr->move_to(board_offset_x, board_offset_y + board_size);
+  cr->line_to(board_offset_x + board_size, board_offset_y + board_size);
+  cr->line_to(board_offset_x + board_size, board_offset_y);
   cr->stroke();
 
   return true;
